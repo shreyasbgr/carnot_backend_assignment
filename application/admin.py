502eeed99c9a9f89e74811ac33cc1c4f4cb5cc52
Shreyas Banagar <shreyasbanagar7@gmail.com>
2021-07-27 02:43:07 +0530

    Show the id field for the student model in the admin panel

--- application/admin.py
+++ application/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Student, School, Book
 
+
 # Register your models here.
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(School)
 admin.site.register(Book)
