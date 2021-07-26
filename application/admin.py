b5a80e6e0d65aac737cbc10d440c94c0b26179f4
Shreyas Banagar <shreyasbanagar7@gmail.com>
2021-07-27 02:01:08 +0530

    Registered models in the admin panel

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student, School, Book
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(School)
+admin.site.register(Book)
