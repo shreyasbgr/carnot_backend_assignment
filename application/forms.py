bf0a9e95231367cdd18b320b337ac8210eb225ae
Shreyas Banagar <shreyasbanagar7@gmail.com>
2021-07-28 00:05:52 +0530

    Set the required attribute to True for student_id in form

--- application/forms.py
+++ application/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class StudentSearchForm(forms.Form):
-    student_id = forms.IntegerField(help_text='Enter the id of the student', required=False)
+    student_id = forms.IntegerField(help_text='Enter the id of the student', required=True)
     student_name = forms.CharField(help_text='Enter the full name of the student', required=False)
