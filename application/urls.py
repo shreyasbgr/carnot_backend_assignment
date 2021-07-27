8749a7685321a91c004e03561185c81c03f0e1fc
Shreyas Banagar <shreyasbanagar7@gmail.com>
2021-07-27 11:43:56 +0530

    Add /application/students route

--- application/urls.py
+++ application/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.helloApplication, name='Application index'),
+    path('', views.hello_application, name='Application index'),
+    path('students/',views.StudentListView.as_view(), name='students'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+
+from .models import Student
 
 
 # Create your views here.
-def helloApplication(request):
+def hello_application(request):
     return HttpResponse('Hello from the application!')
+
+
+class StudentListView(generic.ListView):
+    model = Student
+
