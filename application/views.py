90dd84933e8cc8f9297f23b1ceb87f2e6fafb1ac
Shreyas Banagar <shreyasbanagar7@gmail.com>
2021-07-27 10:44:06 +0530

    Add sample url in the application directory

--- application/views.py
+++ application/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def helloApplication(request):
+    return HttpResponse('Hello from the application!')
