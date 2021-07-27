b3bff8e04123e3780cd75b7f61772ca729352e44
Shreyas Banagar <shreyasbanagar7@gmail.com>
2021-07-28 00:53:30 +0530

    Changes to student_search_form

--- application/forms.py
+++ application/forms.py
@@ -3,4 +3,5 @@ from django import forms
 
 class StudentSearchForm(forms.Form):
     student_id = forms.IntegerField(help_text='Enter the id of the student', required=False, blank=True, null=True)
-    student_name = forms.CharField(help_text='Enter the first name of the student', required=False, blank=True, null=True)
+    student_name = forms.CharField(help_text='Enter the first name of the student', required=False, blank=True,
+                                   null=True)
