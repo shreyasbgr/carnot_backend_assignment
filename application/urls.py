838b2f20cce9373f2e23e2073f1624e06ab33134
Shreyas Banagar <shreyasbanagar7@gmail.com>
2021-07-27 14:43:26 +0530

    Add other information about a student from other tables

--- application/urls.py
+++ application/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.hello_application, name='Application index'),
     path('students/', views.StudentListView.as_view(), name='students'),
-    path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
+    path('student/<int:pk>', views.student_detail_view, name='student-detail'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
-from .models import Student
+from .models import Student, School, Book
 
 
 # Create your views here.
@@ -10,8 +11,12 @@ def hello_application(request):
     return HttpResponse('Hello from the application!')
 
 
-class StudentListView(generic.ListView):
-    model = Student
+def student_detail_view(request, primary_key):
+    student = get_object_or_404(Student, pk=primary_key)
+    school = get_object_or_404(School, student.school)
+    book = get_object_or_404(Book, student.books)
+    return render(request, 'application/student_detail.html', context={'student': student, 'school': school,
+                                                                       'book': book})
 
 
 class StudentDetailView(generic.DetailView):
